30543ac0f47fc272f33c562cbf2507c2aabf33fb
Valentina Prgomet <vprgomet1994@gmail.com>
2023-11-12 21:35:28 +0100

    svi testovi

--- src/test/java/BaseTest.java
+++ src/test/java/BaseTest.java
@@ -11,21 +11,9 @@ import java.util.concurrent.TimeUnit;
 public class BaseTest {
     public String baseUrl = "https://dailytodo.org/";
     public WebDriver driver;
-    @Test
-    public void test() {
-        driver = new ChromeDriver();
-        driver.manage().timeouts().implicitlyWait(2, TimeUnit.SECONDS);
-        driver.manage().window().maximize();
-        driver.get(baseUrl + "create");
-
-        driver.findElement(By.cssSelector("body > div > form > textarea"))
-                .sendKeys("Task");
-
-        driver.findElement(By.cssSelector("body > div > form > p > input")).click();
-    }
 
     @Test
-    public void test2() {
+    public void testCreateTask() {
         driver = new ChromeDriver();
         driver.manage().timeouts().implicitlyWait(2, TimeUnit.SECONDS);
         driver.manage().window().maximize();
@@ -42,6 +30,92 @@ public class BaseTest {
             throw new NoSuchElementException();
     }
 
+
+    @Test
+    public void testCancelSubmit() {
+        driver = new ChromeDriver();
+        driver.manage().timeouts().implicitlyWait(2, TimeUnit.SECONDS);
+        driver.manage().window().maximize();
+        driver.get(baseUrl + "create");
+
+        driver.findElement(By.cssSelector("body > div > form > textarea"))
+                .sendKeys("Task");
+
+        driver.findElement(By.cssSelector("body > div > form > p > a")).click();
+
+        boolean formExists = driver.findElement(By.id("start")).isDisplayed();
+
+       if(!formExists)
+           throw new NoSuchElementException();
+    }
+
+
+
+    @Test
+    public void testCreateTaskAndOverview() {
+        driver = new ChromeDriver();
+        driver.manage().timeouts().implicitlyWait(2, TimeUnit.SECONDS);
+        driver.manage().window().maximize();
+        driver.get(baseUrl + "create");
+
+        driver.findElement(By.cssSelector("body > div > form > textarea"))
+                .sendKeys("Operi veš\nSkuhaj ručak\nZamijeni gume");
+
+        driver.findElement(By.cssSelector("body > div > form > p > input")).click();
+
+        String taskValue = driver.findElement(By.id("t1")).getText();
+
+        if(!taskValue.equals("Operi veš"))
+            throw new NoSuchElementException();
+
+        driver.findElement(By.cssSelector("#tasktable > tbody > tr.t1.tasktr > td.trend.more > a")).click();
+        boolean t = driver.findElement(By.id("tasktable")).isDisplayed();
+
+        if(!t)
+            throw new NoSuchElementException();
+
+        String headline = driver.findElement(By.id("headline")).getText();
+        if(!headline.equals("Operi veš"))
+            throw new NoSuchElementException();
+
+    }
+
+    @Test
+    public void markTodaysTask() throws InterruptedException {
+        driver = new ChromeDriver();
+        driver.manage().timeouts().implicitlyWait(2, TimeUnit.SECONDS);
+        driver.manage().window().maximize();
+        driver.get(baseUrl + "create");
+
+        driver.findElement(By.cssSelector("body > div > form > textarea"))
+                .sendKeys("Operi veš\nSkuhaj ručak\nZamijeni gume");
+
+        driver.findElement(By.cssSelector("body > div > form > p > input")).click();
+
+        String taskValue = driver.findElement(By.id("t1")).getText();
+
+        if(!taskValue.equals("Operi veš"))
+            throw new NoSuchElementException();
+
+        driver.findElement(By.id("check1")).click();
+
+        Thread.sleep(5000);
+
+        boolean isDisplayed = driver.findElement(By.id("check1")).getCssValue("display").equals("none");
+        boolean checked = driver.findElement(By.id("today1")).getCssValue("display").equals("inline");
+        if(!isDisplayed || !checked)
+            throw new NoSuchElementException();
+
+        driver.findElement(By.id("today1")).click();
+
+        Thread.sleep(5000);
+
+        isDisplayed = driver.findElement(By.id("check1")).getCssValue("display").equals("inline-block");
+        checked = driver.findElement(By.id("today1")).getCssValue("display").equals("none");
+        if(!isDisplayed || !checked)
+            throw new NoSuchElementException();
+
+    }
     @BeforeTest
     public void beforeTest() {
         System.out.println("before test");
