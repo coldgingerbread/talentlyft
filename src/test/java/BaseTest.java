0cfa4e5f27d665973c41f330941ce1a1e9a609b2
Valentina Prgomet <vprgomet1994@gmail.com>
2023-11-12 20:42:03 +0100

    prvi test

--- src/test/java/BaseTest.java
+++ src/test/java/BaseTest.java
@@ -1,29 +1,47 @@
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
 
+import java.util.NoSuchElementException;
 import java.util.concurrent.TimeUnit;
 
 public class BaseTest {
-    public String baseUrl = "https://www.javatpoint.com/";
+    public String baseUrl = "https://dailytodo.org/";
     public WebDriver driver;
     @Test
     public void test() {
-// Create driver object for CHROME browser
         driver = new ChromeDriver();
         driver.manage().timeouts().implicitlyWait(2, TimeUnit.SECONDS);
         driver.manage().window().maximize();
-        driver.get(baseUrl);
-// get the current URL of the page
-        String URL= driver.getCurrentUrl();
-        System.out.println(URL);
+        driver.get(baseUrl + "create");
 
-//get the title of the page
-        String title = driver.getTitle();
-        System.out.println(title);
+        driver.findElement(By.cssSelector("body > div > form > textarea"))
+                .sendKeys("Task");
+
+        driver.findElement(By.cssSelector("body > div > form > p > input")).click();
     }
+
+    @Test
+    public void test2() {
+        driver = new ChromeDriver();
+        driver.manage().timeouts().implicitlyWait(2, TimeUnit.SECONDS);
+        driver.manage().window().maximize();
+        driver.get(baseUrl + "create");
+
+        driver.findElement(By.cssSelector("body > div > form > textarea"))
+                .sendKeys("Task");
+
+        driver.findElement(By.cssSelector("body > div > form > p > input")).click();
+
+        String taskValue = driver.findElement(By.id("t1")).getText();
+
+        if(!taskValue.equals("Task"))
+            throw new NoSuchElementException();
+    }
+
     @BeforeTest
     public void beforeTest() {
         System.out.println("before test");
